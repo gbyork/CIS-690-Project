9d8beb2144015f741bb1772d6587dd15d62289ab
Garrett <gbyork18@gmail.com>
2022-03-27 16:18:58 -0500

    Added routes and views. Navbar updated to include these new routes.

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var dashboardRouter = require('./routes/dashboard');
+var statusRouter = require('./routes/status');
+var accountRouter = require('./routes/account');
+
 
 var app = express();
 
@@ -21,8 +24,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/dashboard', dashboardRouter);
+app.use('/status', statusRouter);
+app.use('/account', accountRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
