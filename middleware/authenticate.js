f2bde6f4afb408cd6a8b7b485cb1b72dc6df023c
Sam <sholman@gus.pittstate.edu>
2022-04-21 18:53:51 -0500

    Added middleware and login controller

---
+++ middleware/authenticate.js
@@ -0,0 +1,42 @@
+function ensureAuthenticated(req, res, next) {
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/account/login');
+}
+
+function ensureIsAdmin(req, res, next) {
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated() && res.locals.user.role === 'Admin')
+        return next();
+
+    // if they aren't redirect them to the home page
+    if (req.isAuthenticated) {
+        res.status(401);
+        res.send('You are not authorized to access this page');
+    } else {
+        res.redirect('/account/login');
+    }
+}
+
+function ensureIsAdminOrQualityControl(req, res, next) {
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated() && (res.locals.user.role === 'Admin' || res.locals.user.role === 'Quality Control'))
+        return next();
+
+    // if they aren't redirect them to the home page
+    if (req.isAuthenticated) {
+        res.status(401);
+        res.send('You are not authorized to access this page');
+    } else {
+        res.redirect('/account/login');
+    }
+}
+
+module.exports = {
+    ensureAuthenticated: ensureAuthenticated,
+    ensureIsAdmin: ensureIsAdmin,
+    ensureIsAdminOrQualityControl: ensureIsAdminOrQualityControl
+}
